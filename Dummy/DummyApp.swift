ee1973b4fbdcab0e8f1530ce00299d6e3fca0c09
Kasheeram <102792352+kashee-lv@users.noreply.github.com>
2024-06-21 21:15:07 +0530

    Initial Commit

---
+++ Dummy/DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApp.swift
+//  Dummy
+//
+//  Created by kashee on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
